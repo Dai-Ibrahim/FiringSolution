a3329b3f2da64f2b51c1d9b0e78ecd65c044ee2a
Dai Ibrahim <33212069+Dai-Ibrahim@users.noreply.github.com>
2020-03-02 09:17:25 -0700

    Production Submission

---
+++ Assets/Frog.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+    public float myTimeScale = 1.0f;
+    public GameObject[] targets;
+	public int targetIndex = 0;
+    public float launchForce = 10f;
+	private bool hit;
+	public string targetTag;
+
+
+    Rigidbody rb;
+
+    void Start()
+    {
+		targets = GameObject.FindGameObjectsWithTag(targetTag);
+		Debug.Log(targetIndex);
+
+		Debug.Log(targets[0]);
+
+        Time.timeScale = myTimeScale;
+        rb = GetComponent<Rigidbody>();
+
+        FiringSolution fs = new FiringSolution();
+        Nullable<Vector3> aimVector = fs.Calculate(transform.position, targets[targetIndex].transform.position, launchForce, Physics.gravity);
+
+        if (aimVector.HasValue)
+        {
+            rb.AddForce(aimVector.Value.normalized * launchForce , ForceMode.VelocityChange);
+        }
+    }
+	 void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == targetTag)
+        {
+            hit = true;
+			if(targetIndex < targets.Length-1)
+			{
+				targetIndex++;
+				Reset();
+			}
+			else 
+			{
+				Reset();
+			}
+			
+        }
+		else
+		{
+			hit = false;
+		}
+		Debug.Log(hit);
+
+    }
+
+    public void Reset()
+    {
+        rb.velocity = Vector3.zero;
+        FiringSolution fs = new FiringSolution();
+        Nullable<Vector3> aimVector = fs.Calculate(transform.position, targets[targetIndex].transform.position, launchForce, Physics.gravity);
+
+        if (aimVector.HasValue)
+        {
+            rb.AddForce(aimVector.Value.normalized * launchForce , ForceMode.VelocityChange);
+        }
+    }
+}
+
+   
---
+++ Assets/TargetHit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetHit : MonoBehaviour
+{
+     public GameObject[] targets;
+	 private int targetIndex;
+	 private bool hit;
+
+    private void Start()
+    {
+        targets = GameObject.FindGameObjectsWithTag("Frog");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Frog")
+        {
+            hit = true;
+			
+        }
+		else
+		{
+			hit = false;
+		}
+		Debug.Log(hit);
+    }
+}
